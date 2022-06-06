e27ed98259024d54b438df25e8025a70f497e3fd
Carlos Gutierrez <cgutierrez@utp.edu.co>
2022-06-06 03:33:23 -0500

    add list for estaciones

--- src/app/components/list-estacion/list-estacion.component.ts
+++ src/app/components/list-estacion/list-estacion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudEstacionService } from 'src/app/services/crud-estacion.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-list-estacion',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListEstacionComponent implements OnInit {
 
-  constructor() { }
+  Estaciones:any;
+  constructor(
+    private cruds:CrudEstacionService,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+    this.cruds.getAllEstacion().subscribe(rs=>{
+      //console.log(rs);
+      this.Estaciones=rs;
+    });
+    
+  }
+
+  delEstacion(id:any,i:any){
+    console.log(id,i);
+    if(window.confirm("¿Desea borrar la estacion?")){
+      this.cruds.delEstacion(id).subscribe(rs=>{
+        this.Estaciones.splice(i);
+        this.toastr.success("La estacion se ha eliminado con exito.", "Estacion eliminada!");
+      });
+    }
+    
+      
   }
 
 }
