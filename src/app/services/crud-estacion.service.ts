dd2a9eb2e6afd8a9dc46187c8482ffd0853f031f
Carlos Gutierrez <cgutierrez@utp.edu.co>
2022-06-06 03:18:07 -0500

    add service functions for CRUD with estacion

--- src/app/services/crud-estacion.service.ts
+++ src/app/services/crud-estacion.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { estacion } from './estacion';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CrudEstacionService {
+  API: string="http://localhost/pt-recisa/api/";
+  constructor(private ch:HttpClient) {
 
-  constructor() { }
+  }
+
+  addEstacion(data:estacion):Observable<any>{
+    return this.ch.post(this.API+"?add-estacion",data);
+  }
+
+  editEstacion(id:any,data:any):Observable<any>{
+    return this.ch.post(this.API+"?edit-estacion="+id,data);
+  }
+
+  getAllEstacionr(){
+    return this.ch.get(this.API+"?query-estacion");
+  }
+
+  getEstacion(id:any){
+    return this.ch.get(this.API+"?query-estacion="+id);
+  }
+
+  delEstacion(id:any):Observable<any>{
+    return this.ch.get(this.API+"?del-estacion="+id);
+  }
 }
