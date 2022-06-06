91135de168486d552de5a0c95a8122cd5b199e02
Carlos Gutierrez <cgutierrez@utp.edu.co>
2022-06-06 03:56:45 -0500

    add form for estacion

--- src/app/components/add-estacion/add-estacion.component.ts
+++ src/app/components/add-estacion/add-estacion.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { CrudEstacionService } from '../../services/crud-estacion.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-estacion',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEstacionComponent implements OnInit {
 
-  constructor() { }
+  fg:FormGroup;
+
+  constructor(
+    public fb:FormBuilder, 
+    private cruds:CrudEstacionService, 
+    private rout:Router,
+    private toastr: ToastrService
+  ) {
+    this.fg=this.fb.group({
+      nombre:[''],
+      stockTarjeta:[''],
+      saldoCaja:[''],
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  submitData():any{
+    //console.log("hw!");
+    //console.log(this.fg.value);
+    this.cruds.addEstacion(this.fg.value).subscribe(()=>{
+      this.rout.navigateByUrl('/list-estacion');
+    });
+    this.toastr.success("La estacion se ha agregado con exito.", "Estacion agregada!");
+    
   }
 
 }
